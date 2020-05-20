c4934e819e1bc9e78595fc4715301061d3c015f4
Jabrils <ankelzspam@gmail.com>
2020-05-20 16:42:09 -0700

    init commit

---
+++ DumpAllComments.py
@@ -0,0 +1,47 @@
+import json
+import re
+import urllib.request
+
+from pytube import YouTube
+
+api_key = "ENTER_YOUR_API_KEY_HERE"
+video_id = "arxGmKffjp8"
+nextPageToken = ""
+stop = False
+count = 0
+maxResults = 100 # the max is 100
+
+# 
+if maxResults > 100:
+    maxResults = 100
+
+dump = []
+
+while not stop:
+    url = f"https://www.googleapis.com/youtube/v3/commentThreads?key={api_key}&textFormat=plainText&part=snippet&videoId={video_id}&t&maxResults={maxResults}&pageToken={nextPageToken}"
+
+    json_url = urllib.request.urlopen(url)
+    data = json.loads(json_url.read())
+    nPT = 'nextPageToken'
+
+    # 
+    if nPT in data:
+        nextPageToken = data['nextPageToken']
+    else:
+        stop = True
+
+    # 
+    nComments = len(data['items'])
+
+    # 
+    for i in range(nComments):
+        print(f'Grabbed comment #{i + (count*maxResults)}')
+        put = f"{data['items'][i]['snippet']['topLevelComment']['snippet']['authorDisplayName']}\t{data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay']}".replace('\n'," ").replace('\r'," ")
+        dump.append(put)
+
+    # 
+    count += 1
+
+# 
+with open("allComments.tsv", "w+", encoding='utf-8') as f:
+    f.write("\n".join(dump))
