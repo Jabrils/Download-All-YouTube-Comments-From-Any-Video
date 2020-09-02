a1aeb9b35882591f709507480d7c1e2cc5e1b0f8
Max Walters <max.walters10@gmail.com>
2020-09-02 13:36:28 -0400

    Added Fire CLI functionality

--- DumpAllComments.py
+++ DumpAllComments.py
@@ -1,14 +1,11 @@
 import json
 import re
 import urllib.request
+import fire
 
 from pytube import YouTube
 
 api_key = "ENTER_YOUR_API_KEY_HERE"
-video_id = "arxGmKffjp8"
-nextPageToken = ""
-stop = False
-count = 0
 maxResults = 100 # the max is 100
 
 # 
@@ -17,31 +14,43 @@ if maxResults > 100:
 
 dump = []
 
-while not stop:
-    url = f"https://www.googleapis.com/youtube/v3/commentThreads?key={api_key}&textFormat=plainText&part=snippet&videoId={video_id}&t&maxResults={maxResults}&pageToken={nextPageToken}"
+def getComments(vidID):
+    video_id = vidID
+    nextPageToken = ""
+    stop = False
+    count = 0
 
-    json_url = urllib.request.urlopen(url)
-    data = json.loads(json_url.read())
-    nPT = 'nextPageToken'
+    while not stop:
+        url = f"https://www.googleapis.com/youtube/v3/commentThreads?key={api_key}&textFormat=plainText&part=snippet&videoId={video_id}&t&maxResults={maxResults}&pageToken={nextPageToken}"
+
+        json_url = urllib.request.urlopen(url)
+        data = json.loads(json_url.read())
+        nPT = 'nextPageToken'
+
+        # 
+        if nPT in data:
+            nextPageToken = data['nextPageToken']
+        else:
+            stop = True
+
+        # 
+        nComments = len(data['items'])
+
+        # 
+        for i in range(nComments):
+            print(f'Grabbed comment #{i + (count*maxResults)}')
+            put = f"{data['items'][i]['snippet']['topLevelComment']['snippet']['authorDisplayName']}\t{data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay']}".replace('\n'," ").replace('\r'," ")
+            dump.append(put)
+
+        # 
+        count += 1
 
     # 
-    if nPT in data:
-        nextPageToken = data['nextPageToken']
-    else:
-        stop = True
-
-    # 
-    nComments = len(data['items'])
-
-    # 
-    for i in range(nComments):
-        print(f'Grabbed comment #{i + (count*maxResults)}')
-        put = f"{data['items'][i]['snippet']['topLevelComment']['snippet']['authorDisplayName']}\t{data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay']}".replace('\n'," ").replace('\r'," ")
-        dump.append(put)
-
-    # 
-    count += 1
-
-# 
-with open("allComments.tsv", "w+", encoding='utf-8') as f:
-    f.write("\n".join(dump))
+    with open("allComments.tsv", "w+", encoding='utf-8') as f:
+        f.write("\n".join(dump))
+        
+def main():
+    fire.Fire(getComments)
+    
+if __name__ == '__main__':
+  main()
